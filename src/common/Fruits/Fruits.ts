ccdbeff4ab77a9234173b5ce5ea6d3ea7816409d
shinyoung <totqus130@gmail.com>
2024-02-08 14:03:34 +0900

    suika Game

---
+++ src/common/Fruits/Fruits.ts
@@ -0,0 +1,52 @@
+export const FRUITS = [
+  {
+    name: "00_zero",
+    radius: 33 / 2,
+    color: "#f4f",
+  },
+  {
+    name: "01_one",
+    radius: 48 / 2,
+    color: "#34f",
+  },
+  {
+    name: "02_two",
+    radius: 61 / 2,
+    color: "#84f",
+  },
+  {
+    name: "03_three",
+    radius: 72 / 2,
+    color: "#f66",
+  },
+  {
+    name: "04_four",
+    radius: 89 / 2,
+    color: "#ff4",
+  },
+  {
+    name: "05_five",
+    radius: 114 / 2,
+    color: "#aaa",
+  },
+  {
+    name: "06_six",
+    radius: 129 / 2,
+    color: "#7d7",
+  },
+  {
+    name: "07_seven",
+    radius: 156 / 2,
+    color: "#0a4",
+  },
+  {
+    name: "08_eight",
+    radius: 188 / 2,
+    color: "#fff",
+  },
+  {
+    name: "09_nine",
+    radius: 210 / 2,
+    color: "#077",
+  },
+];
---
+++ src/main.ts
@@ -0,0 +1,191 @@
+import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
+
+import { FRUITS } from "./common/Fruits/Fruits";
+
+const wrap = document.querySelector(".wrap");
+const score = document.querySelector(".score");
+const engine = Engine.create();
+const showRender = Render.create({
+  engine,
+  element: wrap as HTMLElement,
+  options: {
+    wireframes: false,
+    background: "#f7f4c8",
+    width: 850,
+    height: 850,
+  },
+});
+
+const world = engine.world;
+const waitFruit: number[] = [
+  Math.floor(Math.random() * 5),
+  Math.floor(Math.random() * 5),
+];
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: "#e6b143" },
+});
+
+const RightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: "#e6b143" },
+});
+
+const floor = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: { fillStyle: "#e6b143" },
+});
+
+const finishLine = Bodies.rectangle(310, 150, 620, 2, {
+  name: "finishLine",
+  isStatic: true,
+  isSensor: true,
+  render: { fillStyle: "#e6b143" },
+});
+
+const showWaitFruitBox = Bodies.circle(735, 150, 80, {
+  isStatic: true,
+  render: {
+    strokeStyle: "green",
+    fillStyle: "transparent",
+    lineWidth: 2,
+  },
+});
+
+World.add(world, [leftWall, RightWall, floor, finishLine, showWaitFruitBox]);
+
+Render.run(showRender);
+Runner.run(engine);
+let currentBody: any = null;
+let currentFruit: any = null;
+let nextBody: any = null;
+let disableAction = false;
+let interval: number | undefined = undefined;
+
+const addFruit = () => {
+  const index = waitFruit.pop() as number;
+  const fruit = FRUITS[index];
+
+  const body: Body = Bodies.circle(300, 50, fruit.radius, {
+    restitution: 0.2,
+    isSleeping: true,
+    index: index,
+    render: {
+      sprite: { texture: ``, xScale: 1, yScale: 1 },
+      fillStyle: fruit.color,
+    },
+  });
+
+  const Nbody = Bodies.circle(735, 150, FRUITS[waitFruit[0]].radius, {
+    isStatic: true,
+    render: {
+      sprite: { texture: ``, xScale: 1, yScale: 1 },
+      fillStyle: FRUITS[waitFruit[0]].color,
+    },
+  });
+
+  currentBody = body;
+  currentFruit = fruit;
+
+  nextBody = Nbody;
+
+  World.add(world, [body, nextBody]);
+};
+
+window.onkeydown = (event: KeyboardEvent) => {
+  if (currentBody === null || disableAction) return;
+  switch (event.code) {
+    case "KeyA":
+      if (interval) return;
+      interval = setInterval(() => {
+        if ((currentBody?.position.x as number) - currentFruit.radius > 30) {
+          Body.setPosition(currentBody, {
+            x: currentBody.position.x - 1,
+            y: currentBody.position.y,
+          });
+        }
+      }, 5);
+      break;
+
+    case "KeyD":
+      if (interval) return;
+
+      interval = setInterval(() => {
+        if ((currentBody?.position.x as number) + currentFruit.radius < 590) {
+          Body.setPosition(currentBody, {
+            x: currentBody.position.x + 1,
+            y: currentBody.position.y,
+          });
+        }
+      }, 5);
+      break;
+    case "Space":
+      currentBody.isSleeping = false;
+      disableAction = true;
+      clearInterval(interval);
+      interval = undefined;
+      waitFruit.unshift(Math.floor(Math.random() * 5));
+
+      setTimeout(() => {
+        World.remove(world, nextBody);
+
+        addFruit();
+        disableAction = false;
+      }, 1000);
+      break;
+  }
+};
+
+window.onkeyup = (event: KeyboardEvent) => {
+  switch (event.code) {
+    case "KeyA":
+    case "KeyD":
+      clearInterval(interval);
+      interval = undefined;
+      break;
+  }
+};
+
+Events.on(engine, "collisionStart", (event) => {
+  console.log([...event.pairs]);
+  event.pairs.forEach((collision: any) => {
+    if (collision.bodyA.index === collision.bodyB.index) {
+      const index = collision.bodyA.index;
+
+      World.remove(world, [collision.bodyA, collision.bodyB]);
+
+      const newFruit = FRUITS[index + 1];
+
+      const newBody = Bodies.circle(
+        collision.collision.supports[0].x,
+        collision.collision.supports[0].y,
+        newFruit.radius,
+        {
+          render: {
+            sprite: { texture: ``, xScale: 1, yScale: 1 },
+            fillStyle: newFruit.color,
+          },
+          index: index + 1,
+        }
+      );
+
+      World.add(world, newBody);
+
+      if (!!score)
+        score.innerHTML = `${(index + 1) * 100 + parseInt(score.innerHTML)}`;
+    }
+
+    if (
+      (!disableAction && collision.bodyA.name === "finishLine") ||
+      collision.bodyB.name === "finishLine"
+    ) {
+      const restart = confirm("다시 하시겠습니까?");
+      if (restart) {
+        window.location.reload();
+      }
+    }
+  });
+});
+
+addFruit();
